7c242511623f5761a067d8de6c7d188aee21c835
stephen-daq <sdaquila@caltech.edu>
2024-07-14 04:16:20 -0400

    Refactor directory

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import discord
 from discord.ext import commands
 
-from datetime import time, datetime, timedelta
-from bot_key import key
+from datetime import date, time, datetime, timedelta
+
 from time import sleep
+
 import asyncio
+
+import json
+
+import numpy as np
+
+with open("key.json") as file:
+	data = json.load(file)
+	key = data["key"]
 
 intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(command_prefix = '$', intents=intents)
-do_task = True
-WHEN = time(4, 49, 0)
-channel_id = 1
+
+WHEN = (datetime.utcnow() + timedelta(seconds = 3)).time()#time(18,49,0) #
 
 async def daily_task():
+
 	for guild in bot.guilds:
+
 		channels = guild.voice_channels
+
 		max_members = 0
 
 		for c in channels:
+
 			if len(c.members) > max_members:
+
 				max_members = len(c.members)
 				max_channel = c
 
@@ -28,23 +41,22 @@ async def daily_task():
 			member = max_channel.members[0]
 			await play_message(member)
 
-
 @bot.event
 async def on_ready():
 	await background_task()
 
-
 async def play_message(member):
 	channel = member.voice.channel
 	vc = await channel.connect()
-	vc.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source="its 1049 pm.mp3"))
+	audio = discord.FFmpegPCMAudio(executable="ffmpeg-master-latest-win64-gpl\\bin\\ffmpeg", source="its 1049 pm.mp3")
+	vc.play(audio)
 	while vc.is_playing():
 		sleep(0.5)
-	sleep(0.5)
+	sleep(1)
 	await vc.disconnect()
 
-
 async def background_task():
+
     now = datetime.utcnow()
 
     if now.time() > WHEN:
@@ -52,7 +64,8 @@ async def background_task():
         seconds = (tomorrow - now).total_seconds()  # Seconds until tomorrow (midnight)
         await asyncio.sleep(seconds)   # Sleep until tomorrow and then the loop will start 
 
-    while do_task:
+    while True:
+
         now = datetime.utcnow()
         target_time = datetime.combine(now.date(), WHEN)
         seconds_until_target = (target_time - now).total_seconds()
@@ -66,5 +79,4 @@ async def background_task():
 
 
 if __name__ == "__main__":
-	print(bot.guilds)
 	bot.run(key)
